08a5277eaf75b0d3cedbc4e40c6a2b4ea19ac4d1
Gregor Kappler <gregor.kappler@bictxt.de>
2019-11-09 12:22:29 +0100

    Tries factored out into package

--- src/Tries.jl
+++ src/Tries.jl
@@ -1,5 +1,107 @@
 module Tries
 
-greet() = print("Hello World!")
+import Base: get!, show, get, isempty, haskey, setindex!, getindex, pairs, keys, values
+import AbstractTrees
+import AbstractTrees: children, printnode, PreOrderDFS, print_tree
+
+export Trie, subtrie
+struct Trie{K,T}
+    value::Union{Missing,T}
+    nodes::Dict{K,Trie{K,T}}
+end
+struct SubTrie{K,T}
+    path::NTuple{N,K} where N
+    value::Trie{K,T}
+end
+function Base.get(x::Trie{K,T})::T where {K,T}
+    x.value
+end
+Trie{K,T}() where {K,T} = Trie{K,T}(missing, Dict{K,Trie{K,T}}())
+Trie{K,T}(value) where {K,T} = Trie{K,T}(value, Dict{K,Trie{K,T}}())
+Base.get!(x::Trie{K,T},k) where {K,T} =
+    subtrie!(x, k).value
+Base.show(io::IO, x::Trie) =
+    print_tree(io,x)
+Base.get!(f::Function, x::Trie{K,T}, k) where {K,T} =
+    subtrie!(f, x, k...).value
+Base.get(x::Trie{K,T}, k) where {K,T} =
+    subtrie(x, k...).value
+Base.isempty(x::Trie) =
+    isempty(x.nodes)
+
+Base.haskey(x::Trie,k) =
+    isempty(k) || ( haskey(x.nodes,k[1]) && ( length(k)==1 || haskey(x[k[1]],k[2:end]) ) )
+
+subtrie!(x::Trie{K,T},path...) where {K,T} =
+    subtrie!(()->missing, x,path...)
+function subtrie!(f::Function,x::Trie{K,T},path...) where {K,T}
+    isempty(path) && return x
+    x_::Trie{K,T} = x
+    for k::K in path[1:end-1]
+        x_ = get!(() -> Trie{K,T}(),
+                  x_.nodes, k)
+    end
+    ##if length(path) >= 1
+    x_ = get!(() -> Trie{K,T}( f()), x_.nodes, path[end])
+    ##end
+    x_
+end
+function subtrie(x::Trie{K,T},path::Vararg) where {K,T}
+    x_::Trie{K,T} = x
+    for k in path
+        !(haskey(x_.nodes,k)) &&  @warn "no key $k" collect(keys(x_.nodes)) # k haskey(x_.nodes,k) x_.nodes
+        x_ = x_.nodes[k]
+    end
+    x_
+end
+
+import Base.setindex!
+"returns previous value or missing"
+function Base.setindex!(x::Trie{K,T}, v, path...) where {K,T}
+    x_=subtrie!(x,path[1:end-1]...)
+    leaf=subtrie!(x_,path[end])
+    x_.nodes[path[end]] = Trie{K,T}(v,leaf.nodes)
+    leaf.value
+end
+
+function Base.getindex(x::Trie{K,T}, path::Vararg) where {K,T}
+    subtrie(x,path...)
+end
+AbstractTrees.children(x::SubTrie{K,V}) where {K,V} =
+    [ SubTrie{K,V}(tuple(x.path..., k), v) for (k,v) in pairs(x.value.nodes) ]
+function AbstractTrees.printnode(io::IO, x::SubTrie{K,V}) where {K,V}
+    print(io,string(x.path[end:end]))
+    ## value(x.value) !== missing && println(io,":",value(x.value))
+end
+AbstractTrees.children(x::Trie{K,V}) where {K,V} =
+    [ SubTrie{K,V}(tuple(k), v) for (k,v) in pairs(x.nodes) ]
+AbstractTrees.printnode(io::IO, x::Trie) = print(io,"TypeDB") ## error("should print key")
+
+
+function Base.pairs(x::Trie{K,V}; self=true) where {K,V}
+    @assert self
+    ( x.path => x.value for x in PreOrderDFS(SubTrie{K,V}(tuple(),x)) )
+end
+
+Base.keys(x::Trie{K,V}) where {K,V} =
+    (  kv.first for kv in pairs(x)
+       if get(kv.second) !== missing )
+Base.values(x::Trie{K,V}) where {K,V} =
+    ( get(kv.second) for kv in pairs(x)
+      if get(kv.second) !== missing )
+
+## duplicated code!
+function subtrie_key(x::Trie{Pair{Symbol, Any}}, key::Symbol)
+    for (k,v) in x.nodes
+        k.first==key && return v
+    end
+    error("key $key not found")
+end
+function subtrie_value(x::Trie{Pair{Symbol, Any}}, key::Symbol)
+    for (k,v) in x.nodes
+        k.second==key && return v
+    end
+    error("key $key not found")
+end
 
 end # module
